64cd61087b32e4559ef2022b30fb6b1348807be4
louisqvo <louisqvo@gmail.com>
2018-10-06 16:30:57 -0400

    project update

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var projects = {
-    name: ["Bamazon", "Budget Tracker", "Crystal Game", "Friend Finder", "Giphy",
+    name: [
+        "Bamazon", "Budget Tracker", "Burger App","Crystal Game", "Friend Finder", "Giphy",
         "Hangman", "Liri", "Node Word Guess", "Psychic", "Round Me", "Star Wars RPG",
-        "Train Schedule", "Comic Trivia", "Two Player RPS"],
+        "Train Schedule", "Comic Trivia", "Two Player RPS", "Toronto Star Sports"
+    ],
     website: ["https://volouis.github.io/bamazon/",
         "https://cryptic-badlands-31521.herokuapp.com/",
+        "https://github.com/volouis/burger",
         "https://volouis.github.io/unit-4-game/",
         "https://floating-lake-72754.herokuapp.com/",
         "https://volouis.github.io/Giphy/",
@@ -14,28 +17,38 @@ var projects = {
         "https://volouis.github.io/Star-War-RPG-game/",
         "https://volouis.github.io/TrainScheduler/",
         "https://volouis.github.io/TriviaGame/",
-        "https://volouis.github.io/RPSTwoPlayers/"],
-    img: ["Bamazon", "Budget", "Crystal", "FriendFinder", "Giphy",
+        "https://volouis.github.io/RPSTwoPlayers/",
+        "https://volouis.github.io/RPSTwoPlayers/",
+        "https://github.com/volouis/mongoCheerio"
+    ],
+    img: [
+        "Bamazon", "Budget", "Burger","Crystal", "FriendFinder", "Giphy",
     "Hangman", "Liri", "node_wordGuess", "Psychic", "RoundMe", "StarWars",
-    "TrainSchedule", "Trivia", "TwoPlayerRPS"]
+    "TrainSchedule", "Trivia", "TwoPlayerRPS","TorontoStarSport"
+],
+    applications: [
+        "Javascript, Node.js",
+        "Ajax, CSS, HTML, Javascript, JQuery, MySQL, Node.js",
+        "HTML, Javascript, JQuery, MySQL, Node.js",
+        "CSS, HTML, Javascript",
+        "Ajax, Bootstrap, HTML, Javascript, JQuery, Node.js",
+        "Ajax, CSS, HTML, Javascript, JQuery",
+        "CSS, HTML, Javascript, JQuery",
+        "HTML, Javascript, Node.js",
+        "Javascript, Node.js",
+        "CSS, HTML, Javascript",
+        "Ajax, CSS, HTML, Javascript, JQuery",
+        "CSS, HTML, Javascript, JQuery",
+        "Bootstrap, CSS, Firebase, HTML, Javascript, JQuery",
+        "Bootstrap, CSS, HTML, Javascript, JQuery",
+        "CSS, Firebase, HTML, Javascript, JQuery",
+        "Ajax, Bootstrap, CSS, HTML, Javascript, JQuery, Mongo, Node.js"
+    ]
+
 }
 
 $(document).ready(function(){
     $(".proName").hide();
-
-    // for(var i = 0; i < projects.name.length; i++){
-
-    //     var proj = $("<div class=proj>");
-    //     var btn = $("<a href=" + projects.website[i] + ">");
-    //     var btnDo = $ ("<div class=projImg data-val=" + i +">");
-    //     btnDo.append("<img class=\"proImg pro1\" src=img/" + projects.img[i] + ".png>");
-    //     var hov = $("<div class=\"proName name1\">");
-    //     hov.append("<h2 class=\"proWord\">" + projects.name[i]);
-    //     hov.append("<h5 class=\"proWord\">View Detail");
-    //     btnDo.append(hov);
-    //     btn.append(btnDo);
-    //     proj.append(btn)
-    // }
 
 
     $(".projects").append(proj)
@@ -51,21 +64,8 @@ $(document).ready(function(){
         $(".proImg").fadeTo("slow", 1)
         $(".proName").fadeTo("slow", 0)
     });
+    
 });
-
-
-{/* <div class="proj"> 
-    <a href="https://volouis.github.io/Psychic-Game/">
-        <div class="projImg" data-val="1">
-            <img class="proImg pro1" src="img/Psychic.png" alt="...">
-            <div class="proName name1">
-                <h2 class="proWord">Psychic</h2>
-                <h5 class="proWord">View Detail</h5>
-            </div>
-        </div>
-    </a>
-</div> */}
-
 
 
 $(".projImg").hover(function(){
@@ -80,3 +80,27 @@ $(".projImg").hover(function(){
     $(".proImg").fadeTo("slow", 1)
     $(".proName").fadeTo("slow", 0)
 });
+
+for(var i = 0; i < projects.name.length; i++){
+    var btn = $(`<a href=${projects.website[i]}>`)
+    var card = $(`<div class="card">`)
+    var img = $(`<div class="img-container">`)
+    var content = $(`<div class="content">`)
+    var list = $("<ul>")
+    
+    var pic = $(`<img>`)
+    pic.attr("alt", projects.name[i]);
+    pic.attr("src", "img/" + projects.img[i] + ".png");
+    
+    list.append("<li><strong>Name:</strong> " + projects.name[i])
+    list.append("<li><strong>Application:</strong> " + projects.applications[i])
+    
+    content.append(list);
+    img.append(pic);
+    
+    card.append(img);
+    card.append(content);
+
+    btn.append(card)
+    $(".wrapper").append(btn)
+}
