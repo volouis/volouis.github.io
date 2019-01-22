b5e6a09df9ce05629e4cadc216e45950a1f9e8e6
louisqvo <louisqvo@gmail.com>
2019-01-22 17:27:42 -0500

    update

--- app.js
+++ app.js
@@ -189,20 +189,3 @@ function popUp(num){
     
 }
 
-
-var hare = 5;
-var tortoise = 11;
-var j = 0;
-
-for(j=0;j<20;j++)
-{
- if(hare < tortoise)
-   hare *= 2;
- else if(hare == tortoise)
-   break;
- else
-   tortoise += 1;
-   console.log(hare, tortoise)
-}
-
-console.log(hare+tortoise)
